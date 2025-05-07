6af909f32117754cdbfb7107baf9f13947079c8e
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-05-07 22:21:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "7107488581:AAFAq0temUKSCH2Ou7RE5n9bfRt4HoV
 
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002523462080"))
-ADMINS = int(environ.get("ADMINS", "7348205141 1249672673"))
+ADMINS = int(environ.get("ADMINS", ""))
 OWNER_ID = environ.get("OWNER_ID","https://t.me/BotMaster55") # Yaha owner link do with https://
 JOIN_CHANNEL = environ.get("JOIN_CHANNEL","-1002523462080") # Yaha channel link do with https://t.me
 UPDATE_CHANNEL = environ.get("UPDATE_CHANNEL","-1002523462080") # With https://
