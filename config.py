079e6759a51647e0f3a846b8892a06bc2b62cfe5
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-05-07 21:42:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 ADMINS = int(environ.get("ADMINS", "7348205141 1249672673"))
-OWNER_ID = environ.get("OWNER_ID","https://t.me/BotMaster55")
-JOIN_CHANNEL = environ.get("JOIN_CHANNEL","")
-UPDATE_CHANNEL = environ.get("UPDATE_CHANNEL","")
+OWNER_ID = environ.get("OWNER_ID","https://t.me/BotMaster55") # Yaha owner link do with https://
+JOIN_CHANNEL = environ.get("JOIN_CHANNEL","") # Yaha channel link do with https://t.me
+UPDATE_CHANNEL = environ.get("UPDATE_CHANNEL","") # With https://
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
